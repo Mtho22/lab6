4d6660953ca419071fac86fed47c7b21f10fd9df
Mtho22 <131072595+Mtho22@users.noreply.github.com>
2024-04-11 15:30:44 +0200

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -1,33 +1,24 @@
-const cars = require('./cars.json');
-module.exports = async function (context, req) {
+module.exports = async function processRequest(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.method==="GET"){
+    if (req.method === "GET") {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            add:"massambe",
+            add: "mtho",
+            body: cars
+        };
+    } else {
+        if (req.body.func === 1) {
+            cars.push(req.body.car);
+        } else {
+            const id = req.body.id;
+            const index = cars.findIndex(car => car.id === id);
+            if (index !== -1) {
+                cars.splice(index, 1);
+            }
+        }
+        context.res = {
+            add: "mtho",
             body: cars
         };
     }
-    else{
-        if (req.body.func===1){
-            cars.push(req.body.car);
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                add:"massambe",
-                body: cars
-            };
-        }
-        else{
-            let id=req.body.id;
-            const index = cars.findIndex(car => car.id === id);
-            cars.splice(index, 1);
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                add:"massambe",
-                body: cars
-            };
-        }
-    }
-    
-}
+};
